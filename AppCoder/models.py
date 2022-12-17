cf831466d110fa83830ba64b225e6c62e3de0530
gastonmauricios <gastonmauricios@gmail.com>
2022-12-17 20:00:10 -0300

    mi primer portafolio

---
+++ AppCoder/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+# Create your models here. # ESTOS SON LOS MODELOS DE TABLAS DE BASE DE DATOS, los objetos se crean en un views
+class Curso(models.Model): # es una herencia objeto Clase curso tiene un models, campos para llenar cosas.
+  
+    nombre=models.CharField(max_length=50) # campo de texto , creo objetos!!
+    comision=models.IntegerField()
+
+class Estudiante(models.Model): # es una herencia objeto
+    
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    email=models.EmailField()
+   
+class Profesor(models.Model): # es una herencia objeto
+    
+    nombre=models.CharField(max_length=50)
+    apellido=models.CharField(max_length=50)
+    email=models.EmailField()
+    profesion=models.IntegerField(50)
+       
+class Entregable(models.Model): # es una herencia objeto
+    
+    nombre=models.CharField(max_length=50) # campo de texto
+    fecha_de_entrega=models.DateField()
+    entregado=models.BooleanField() # si o no true o false
+
+
+
+
+
+
+  
